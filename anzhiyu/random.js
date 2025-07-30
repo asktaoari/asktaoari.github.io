93a12824a532fbd3209f3758b6760d1a24531977
asktao <836543356@qq.com>
2025-07-30 22:06:19 +0800

    Site updated: 2025-07-30 22:06:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/12/1.思索很久还是入坑了/"];function toRandomPost(){
+var posts=["2025/07/12/1.思索很久还是入坑了/","2025/07/12/2.浮生/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
